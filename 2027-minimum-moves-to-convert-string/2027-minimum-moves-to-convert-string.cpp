8ca89df3696f7302df548cbe53eee17a0601b559
Narayan Kakade <161179703+narayan1729@users.noreply.github.com>
2026-01-30 02:13:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (65.22%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int minimumMoves(string s) {
-        int moves = 0;
-        for(int i = 0; i < s.length(); ++i)
-        {
-            if(s[i] == 'X')
-            {
-                ++moves;
+        int ans=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='X'){
+                ans++;
                 i+=2;
             }
         }
-        
-        return moves;
+        return ans;
     }
 };
